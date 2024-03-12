3fc885696c14714341820f74866df67645edad83
cherrycheeks <dirtiestcontext@gmail.com>
2024-03-11 23:34:31 -0400

    I should probably work on UML more first.

--- DirtiestContext - Dice API/src/main/groovy/com/dirtiestcontext/dice_api/controllers/RollController.groovy
+++ DirtiestContext - Dice API/src/main/groovy/com/dirtiestcontext/dice_api/controllers/RollController.groovy
@@ -1,9 +1,30 @@
 package com.dirtiestcontext.dice_api.controllers
 
+import com.dirtiestcontext.dice_api.requests.RollRequest
+import com.dirtiestcontext.dice_api.responses.RollResponse
+import com.dirtiestcontext.dice_api.services.RollService
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
+//research MDC
 @Slf4j
 @Controller
 class RollController {
+
+	@Autowired
+	RollService rollService
+
+	@PostMapping
+	ResponseEntity<RollResponse> createRoll(@RequestBody RollRequest rollRequest) {
+		log.info('Creating Roll')
+		ResponseEntity<RollResponse> response
+	}
+
+	@GetMapping
+
 }
--- DirtiestContext - Dice API/src/main/groovy/com/dirtiestcontext/dice_api/services/RollService.groovy
+++ DirtiestContext - Dice API/src/main/groovy/com/dirtiestcontext/dice_api/services/RollService.groovy
@@ -2,14 +2,19 @@ package com.dirtiestcontext.dice_api.services
 
 import com.dirtiestcontext.dice_api.models.Roll
 import com.dirtiestcontext.dice_api.requests.RollRequest
+import com.dirtiestcontext.dice_api.responses.RollResponse
 import groovy.util.logging.Slf4j
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @Slf4j
 @Service
 class RollService {
 
 	//create
+
 	//read
 	//update
 	//delete
